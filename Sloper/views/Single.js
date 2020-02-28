27f274e7dcf670195f64d68a4c01287f4ad66541
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-02-28 09:51:46 +0200

    username added to comments

--- Sloper/views/Single.js
+++ Sloper/views/Single.js
@@ -49,7 +49,7 @@ const Single = props => {
           comments.map(async i => {
             const user = await fetchAPI('GET', 'users', i.user_id, token)
             i.username = user.username;
-            return user
+
           })
         )
         setComments(comments);
